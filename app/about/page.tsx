3ecbd1470009f780258e16373c37e06e8e32ec3a
Gwendolyn <wilkistagwendolyn@gmail.com>
2024-11-09 22:51:32 +0300

    style: chnage ui

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import React from 'react'
+import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
@@ -8,68 +8,70 @@ import {
   CarouselNext,
   CarouselPrevious,
   type CarouselApi,
-} from "@/components/ui/carousel";
+} from "@/components/ui/carousel"
 
 export default function About() {
-  const imageLinks = [
-    {
-      src: "/images/5AM-Challenge.jpg",
-      alt: "Image 1",
-    },
-    {
-      src: "/images/5AM-Challenge.jpg",
-      alt: "Image 1",
-    },
-    {
-      src: "/images/5AM-Challenge.jpg",
-      alt: "Image 1",
-    },
-    {
-      src: "/images/5AM-Challenge.jpg",
-      alt: "Image 1",
-    },
-  ];
-  const [api, setApi] = React.useState<CarouselApi>();
-  const [current, setCurrent] = React.useState(0);
-  const [count, setCount] = React.useState(0);
+    const imageLinks = [
+        {
+            src: '/images/5AM-Challenge.jpg',
+            alt: 'Image 1',
+        },
+        {
+            src: '/images/5AM-Challenge.jpg',
+            alt: 'Image 1',
+        },
+        {
+            src: '/images/5AM-Challenge.jpg',
+            alt: 'Image 1',
+        },
+        {
+            src: '/images/5AM-Challenge.jpg',
+            alt: 'Image 1',
+        },
+    ];
+    const [api, setApi] = React.useState<CarouselApi>()
+    const [current, setCurrent] = React.useState(0)
+    const [count, setCount] = React.useState(0)
+   
+    React.useEffect(() => {
+      if (!api) {
+        return
+      }
+   
+      setCount(api.scrollSnapList().length)
+      setCurrent(api.selectedScrollSnap() + 1)
+   
+      api.on("select", () => {
+        setCurrent(api.selectedScrollSnap() + 1)
+      })
+    }, [api])
 
-  React.useEffect(() => {
-    if (!api) {
-      return;
-    }
 
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
+    return (
+     <div className='px-52 pt-10'>
 
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
+    <h3 className='text-white text-6xl font-bold'>About Me</h3>
 
-  return (
-    <div className="px-52 pt-10">
-      <h3 className="text-white text-6xl font-bold">About Me</h3>
-
-      <div className="mx-auto">
-        <Carousel setApi={setApi} className="w-full max-w-xs">
-          <CarouselContent>
-            {imageLinks.map((item: { src: string }, key: number) => (
-              <CarouselItem key={key}>
+    <div className="mx-auto ">
+      <Carousel setApi={setApi} className="w-full max-w-xs">
+        <CarouselContent>
+            {imageLinks.map((item: any, key: number) => (
+                <CarouselItem key={key}>
                 <Card>
-                  <CardContent className="object-cover w-full h-full">
-                    <img src={item.src} alt={`Slide ${key}`} />
+                  <CardContent className=" object-cover w-full h-full">
+                    <img src={item.src} alt="" />
                   </CardContent>
                 </Card>
               </CarouselItem>
             ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-        <div className="py-2 text-sm text-muted-foreground">
-          Slide {current} of {count}
-        </div>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="py-2 text-sm text-muted-foreground">
+        Slide {current} of {count}
       </div>
     </div>
-  );
-}
+     </div>
+    );
+  }
